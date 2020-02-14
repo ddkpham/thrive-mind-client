c12ee1ba6930dd4b37b45231cda15bf34c2912d9
Andrew Song <andrewgnos@gmail.com>
2020-02-13 19:15:39 -0800

    fix login api

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,2 +1,2 @@
 // wraps the fetch API to handle the correct API endpoint
-export default (url, options) => fetch(`${process.env.REACT_APP_API}${url}`, options = { method: 'GET' })
+export default (url, options = { method: 'GET' }) => fetch(`${process.env.REACT_APP_API}${url}`, options)

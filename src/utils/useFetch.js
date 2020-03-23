d62ce1f2bf1ce69a0d5856c6bccf7e3db733756e
David Pham <ddkpham@gmail.com>
2020-03-22 20:22:30 -0700

    hook up auth token to request headers

--- src/utils/useFetch.js
+++ src/utils/useFetch.js
@@ -1,25 +1,22 @@
 // provides a hook to use an authenticated fetch API outside of redux
-import { useSelector } from 'react-redux'
-import fetch from './fetch'
+import { useSelector } from "react-redux";
+import fetch from "./fetch";
 
 const useFetch = () => {
-  const { token } = useSelector(({ app }) => app)
+  const { token } = useSelector(({ app }) => app);
 
-  return (url, options = { method: 'GET' }) => {
+  return (url, options = { method: "GET" }) => {
     if (options.headers) {
-      options.headers['x-access-token'] = token
+      options.headers["Authorization"] = token;
     } else {
-      options.headers = { 'x-access-token': token }
+      options.headers = { Authorization: token };
     }
 
     // TODO: handle unauthenticated API
     // ie. if session expired, redirect user to login page
 
-    return fetch(
-      url,
-      options,
-    )
-  }
-}
+    return fetch(url, options);
+  };
+};
 
-export default useFetch
+export default useFetch;

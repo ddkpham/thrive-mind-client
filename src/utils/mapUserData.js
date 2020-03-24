a730300d5c935325376ce4b8c86e7c92a9767a1f
Andrew Song <andrewgnos@gmail.com>
2020-03-24 01:01:02 -0700

    default missing profile fields to empty strings

--- src/utils/mapUserData.js
+++ src/utils/mapUserData.js
@@ -7,6 +7,10 @@ export default (user) => {
     is_seeking: user['custom:is_seeking'],
     last_name: user.family_name,
     email_address: user.email,
+    medical_history: '',
+    current_prescription: '',
+    preferences: '',
+    health_care_plan: '',
   }
 
   return mappedPayload
